5bdb38ae754db6ed22a397864f749f71bf599003
Atharvatathe <atharva.tathe55@gmail.com>
2025-10-28 12:56:33 +0530

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import ExploreBtn from "./components/ExploreBtn";
+import EventCard from "./components/EventCard";
+import { events } from "@/lib/constants";
+
+const Home = () => {
+  return (
+    <section>
+      <h1 className="text-center">
+        {" "}
+        The Hub For Every Dev
+        <br /> Event You can't Miss
+      </h1>
+      <p className="text-center mt-5">
+        Hackthons, Meetups, and Conferance, All in One Place
+      </p>
+      <ExploreBtn />
+      <div className="mt-20 space-y-7">
+        <h3>Featured Event</h3>
+        <ul className="events">
+          {events.map((event) => (
+            <li key={event.title}>
+              <EventCard {...event} />
+            </li>
+          ))}
+        </ul>
+      </div>
+    </section>
+  );
+};
+export default Home;
