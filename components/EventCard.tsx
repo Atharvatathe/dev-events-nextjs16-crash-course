99ee97e801a0a5ca42b16dbb024f0c16c121fb8a
Atharvatathe <atharva.tathe55@gmail.com>
2025-10-31 11:38:31 +0530

    implement api routes, server actions and caching and the event details page

--- components/EventCard.tsx
+++ components/EventCard.tsx
@@ -12,7 +12,7 @@ interface props {
 
 const EventCard = ({ title, image, slug, location, date, time }: props) => {
   return (
-    <Link href={`/events`} id="event-card">
+    <Link href={`/events/${slug}`} id="event-card">
       <Image
         src={image}
         alt={title}
@@ -20,7 +20,7 @@ const EventCard = ({ title, image, slug, location, date, time }: props) => {
         height={300}
         className="poster"
       />
-      <div className="flex flex-row gap-2">git pull
+      <div className="flex flex-row gap-2">
         <Image src="/icons/pin.svg" alt="location" width={14} height={14} />
         <p>{location}</p>
       </div>
